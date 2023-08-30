63285e8be55ababf38162140c69594e576669d3e
Rodrigo Ferreira <rodrigo_ferreira89@outlook.com>
2023-08-30 17:27:15 -0300

    alterando coisas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 const btnAtivarMenu = document.querySelector(".btn-menu");
+const verDetalhes = document.querySelector(".btn-ver-condominio");
 
 btnAtivarMenu.addEventListener('click', ()=> {
     const menu = document.querySelectorAll('.header-dropdown-menu');
     menu[3].classList.toggle('ativar-menu');
-    console.log(btnAtivarMenu)
 });
 
+verDetalhes.addEventListener('click', ()=> {
+    const detalhes = document.querySelector(".detalhes-div-condominio");
+    detalhes.classList.toggle('ativar-detalhes');
+    
+});
 
 
 
