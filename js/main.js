e6827667595cc77a26dbacfa192f7e2d7051fb10
Rodrigo Ferreira <rodrigo_ferreira89@outlook.com>
2023-09-01 15:00:40 -0300

    final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const btnAtivarMenu = document.querySelector(".btn-menu");
 const verDetalhes = document.querySelector(".btn-ver-condominio");
+const btnDialog = document.querySelector(".btn-ir-form");
+const dialog = document.getElementById("dialog");
+const btnCancelar = document.querySelector(".cancelar");
 
 btnAtivarMenu.addEventListener('click', ()=> {
     const menu = document.querySelectorAll('.header-dropdown-menu');
@@ -9,11 +12,50 @@ btnAtivarMenu.addEventListener('click', ()=> {
 verDetalhes.addEventListener('click', ()=> {
     const detalhes = document.querySelector(".detalhes-div-condominio");
     detalhes.classList.toggle('ativar-detalhes');
-    
+});
+
+
+btnDialog.addEventListener('click', ()=> {
+    dialog.show()
+});
+
+btnCancelar.addEventListener('click', (e)=> {
+    e.preventDefault()
+    dialog.close()
 });
 
 
 
+// window.addEventListener("resize", function() {
+//     const widthResize = this.window.innerWidth;
+//     console.log(widthResize)
+//     if(widthResize < 992) {
+
+//         btnDialog.addEventListener('click', ()=> {
+//             dialog.show()
+//         });
+        
+//         btnCancelar.addEventListener('click', (e)=> {
+//             e.preventDefault()
+//             dialog.close()
+//         });
+//     }
+// })
+
+// window.addEventListener("load", function() {
+//     const width = window.innerWidth;
+//     if (width > 992) {
+//         btnDialog.addEventListener('click', ()=> {
+//             dialog.show()
+//         });
+        
+//         btnCancelar.addEventListener('click', (e)=> {
+//             e.preventDefault()
+//             dialog.close()
+            
+//         });
+//     }
+// });
 
 
 
